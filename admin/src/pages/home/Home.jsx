b638bc3582f16a6ac11ea43e1635e799e5dd7bd6
Liantsoa <liantsoa.rabevahoaka@gmail.com>
2025-02-13 00:44:48 +0300

    add progress bar component + add chart

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import "./Home.scss"
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar"
 import Widget from "../../components/widget/Widget";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
 
 const Home = () => {
   return (
@@ -14,7 +16,13 @@ const Home = () => {
             <Widget type="order" />
             <Widget type="earning" />
             <Widget type="balance" />
-        </div>
+          </div>
+
+          <div className="charts">
+            <Featured />
+            <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+          </div>
+
         </div>
     </div>
   )
